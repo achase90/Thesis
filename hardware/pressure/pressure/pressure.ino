ca20c860de1e563479e039b5b1a5a2193ba5b6bc
Adam Chase <achase90@gmail.com>
2013-07-23 15:48:27 -0700

    got pressure sensors working

--- hardware/pressure/pressure/pressure.ino
+++ hardware/pressure/pressure/pressure.ino
@@ -1,23 +1,36 @@
 void setup() {
-Serial.begin(9600);
-Serial2.begin(9600);
-Serial.println("Serial ports open");
+  Serial.begin(9600);
+  Serial3.begin(9600);
+  Serial.println("Serial ports open");
 }
 
 void loop() {
-char x;
-int nchars;
-  // put your main code here, to run repeatedly: 
-  Serial2.print('RH\r');
-  if (Serial2.available()>0)
-  {
-    while (Serial2.available()>0)
-    {
- x=Serial2.read();
- Serial.println(x,HEX);
+  byte x;
+  byte comm[20];
+  /*comm[0] = 0x57;
+   comm[1] = 0x43;
+   comm[2] = 0x0d;
+   Serial3.write(comm,3);
+   delay(10);
+   */
+  comm[0] = 0x52;
+  comm[1] = 0x48;
+  comm[2] = 0x0d;
 
-}
+  Serial3.write(comm,3);
+
+  // put your main code here, to run repeatedly: 
+  if (Serial3.available()>0)
+  {
+    while (Serial3.available()>0)
+    {
+      x=Serial3.read();
+      Serial.write(x);
+    }
+    Serial.println();
   }
 
-  delay(10);
+  delay(20);
 }
+
+
