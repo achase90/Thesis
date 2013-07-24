57b7a194095dc49f57f489421444ecaab5ceb40a
Adam Chase <achase90@gmail.com>
2013-07-24 10:19:55 -0700

    pressure transducer now outputs data in integer form

--- hardware/pressure/pressure/pressure.ino
+++ hardware/pressure/pressure/pressure.ino
@@ -6,7 +6,10 @@ void setup() {
 
 void loop() {
   byte x;
-  byte comm[20];
+  byte comm[3];
+  char bytesIn[10];
+  int nchars;
+  int16_t pressure;
   /*comm[0] = 0x57;
    comm[1] = 0x43;
    comm[2] = 0x0d;
@@ -18,19 +21,18 @@ void loop() {
   comm[2] = 0x0d;
 
   Serial3.write(comm,3);
+  delay(10);
 
   // put your main code here, to run repeatedly: 
   if (Serial3.available()>0)
   {
-    while (Serial3.available()>0)
-    {
-      x=Serial3.read();
-      Serial.write(x);
-    }
-    Serial.println();
+    nchars = Serial3.readBytesUntil('=',bytesIn,10);
+    nchars = Serial3.readBytesUntil(0x20,bytesIn,10);
+    pressure = (int16_t)strtol(bytesIn,NULL,16);
+    Serial.println(pressure);
   }
-
-  delay(20);
 }
+
+
 
 
