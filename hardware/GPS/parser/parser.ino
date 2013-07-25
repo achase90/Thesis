8ee076cdd03b24f6d9f49190cdb91b32216510e1
Adam Chase <achase90@gmail.com>
2013-07-25 10:09:40 -0700

    switching to atof version of parsing gps data, since it's faster on the arduino due

--- hardware/GPS/parser/parser.ino
+++ hardware/GPS/parser/parser.ino
@@ -17,47 +17,10 @@ void loop() {
    Serial.println();
    
    }
-   // put your main code here, to run repeatedly: 
    }
    */
-  char temp[6];
-  temp[0] = '1';
-  temp[1] = '2';
-  temp[2] = '3';
-  temp[3] = '.';
-  temp[4] = '4';
-  temp [5] = '5';
-int time = micros();
-/* convert string float to fixed point without float (SLOWER THAN USING ATOF)
-  int i=0;
-  int sizeOfTemp = sizeof(temp)/sizeof(char);
-
-  int decLoc;
-  while (i < sizeOfTemp)
-  {
-    if (temp[i] == 0x2e)
-    {
-      decLoc = i;
-      break;
-    }
-    i++;  
-  }
-
-  char tempOut[20] = {0x00};
-  for (int i=0;i<sizeOfTemp-1; i++)
-  {
-    if (i >=decLoc)
-    {
-      tempOut[i] = temp[i+1];
-    }
-    else
-      tempOut[i] = temp[i];
-  }
-  int32_t tempOutInt = pow(10,8-(sizeOfTemp-decLoc))*atoi(tempOut);
-  */
   int32_t tempOutInt = 100000*atof(temp);
-  //Serial.println(tempOutInt);
-  Serial.println(micros()-time);
+  Serial.println(tempOutInt);
 }
 
 
