b3a220297d36b95b80ac07ad56c669aa8c1480db
Adam Chase <achase90@gmail.com>
2013-07-25 17:00:23 -0700

    Cleaned up accel code and fixed ADXL library file

--- hardware/accel/accel.ino
+++ hardware/accel/accel.ino
@@ -4,44 +4,49 @@
 
 ADXL362 xl;
 
-int temp;
-float FS=10000;
 int16_t XValue, YValue, ZValue, Temperature;
 
 
 void setup(){
-    Serial.begin(9600);
-    xl.begin();                   // Setup SPI protocol, issue device soft reset
-    xl.beginMeasure();            // Switch ADXL362 to measure mode  
-    //xl.checkAllControlRegs();     // Burst Read all Control Registers, to check for proper setup
-	byte range = xl.SPIreadOneRegister(0x2c);
-range = (range >> 6) & 00000011;
-Serial.println(range);
-switch (range)
-{
- case 0: FS = 2;break;
- case 1: FS = 4;break;
- case 2: FS = 8;break;
- case 3: FS = 8; break;  
-}
-//FS = 8;
-    Serial.print("\n\nBegin Loop Function:\n");
+  Serial.begin(9600);
+  xl.begin();                   // Setup SPI protocol, issue device soft reset
+  xl.beginMeasure();            // Switch ADXL362 to measure mode  
+  byte range = xl.SPIreadOneRegister(0x2c);
+  range = (range >> 6) & 00000011;
+  Serial.println(range);
+  switch (range)
+  {
+  case 0: 
+    FS = 2;
+    break;
+  case 1: 
+    FS = 4;
+    break;
+  case 2: 
+    FS = 8;
+    break;
+  case 3: 
+    FS = 8; 
+    break;  
+  }
+  FS = 2;
+  Serial.print("\n\nBegin Loop Function:\n");
 }
 
 void loop(){
-    
-    // read all three axis in burst to ensure all measurements correspond to same sample time
-    xl.readXYZTData(XValue, YValue, ZValue, Temperature);  	 
-    //long xData = xl.readXData();
-    //Serial.println(xData);
-        Serial.print('\t');
-    Serial.print(FS*XValue/2048,6);
-    Serial.print('\t');
-        Serial.print(FS*YValue/2048,6);
-    Serial.print('\t');
-        Serial.print(FS*ZValue/2048,6);
-    Serial.print('\t');
-        Serial.println( Temperature);
-    delay(100);                // Arbitrary delay to make serial monitor easier to observe
+
+  // read all three axis in burst to ensure all measurements correspond to same sample time
+  xl.readXYZTData(XValue, YValue, ZValue, Temperature);  	 
+
+  Serial.print('\t');
+  Serial.print(XValue);
+  Serial.print('\t');
+  Serial.print(YValue);
+  Serial.print('\t');
+  Serial.print(ZValue);
+  Serial.print('\t');
+  Serial.println( Temperature);
+  delay(100);                // Arbitrary delay to make serial monitor easier to observe
 }
+
 
