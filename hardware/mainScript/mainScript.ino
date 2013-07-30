c5bf4076c3749f3b1b9d300aa597d835da36b7d5
Adam Chase <achase90@gmail.com>
2013-07-30 12:32:16 -0700

    modified serial port referencing to map to sensors better

--- hardware/mainScript/mainScript.ino
+++ hardware/mainScript/mainScript.ino
@@ -1,17 +1,17 @@
+//TODO: add checking if data is actually available for all sensors, especially serial sensors
+
 #include <Wire.h>
 #include <ITG3200.h>
 #include <SPI.h>
 #include <SD.h>
-//#include <HardwareSerial.h>
-
-HardwareSerial *magSerial;
-HardwareSerial *gpsSerial;
-HardwareSerial *pressSerial;
 
 int magBaud = 9600;
 int gpsBaud = 9600;
 int pressBaud = 9600;
 
+USARTClass &magSerial = Serial1;
+USARTClass &gpsSerial = Serial2;
+USARTClass &pressureSerial = Serial3;
 
 char pressSN0[13] = "4F15-01-A213";
 char pressSN1[13] = "R10F30-04-A1";
@@ -22,27 +22,34 @@ void setup() {
   Serial.begin(9600); //begin serial communication for debugging
   Serial.println("Serial port initialized.");
 
-
-  serialDeviceInit(Serial, Serial1, magBaud,"mag");
-  //serialDeviceInit(Serial, Serial2, gpsBaud,"gps");
-  serialDeviceInit(Serial, Serial3, pressBaud,"pre");
+  serialDeviceInit(Serial, magSerial, magBaud,"mag"); //initialize magnetometer on Serial1
+  //serialDeviceInit(Serial, gpsSerial, gpsBaud,"gps"); //initialize gps on Serial2
+  serialDeviceInit(Serial, pressureSerial, pressBaud,"pre"); //initialize pressure sensors on Serial3
 
 }
 
 void loop() {
-  int16_t pressure[4];
+  int16_t pressure[4]; //pressure sensor data
+  int16_t magReading[3]; //magnetometer data
 
   //start timer
   unsigned long time = millis();
-  //read pressure transducer
-  readAllPress (Serial3,pressSN0,pressSN1,pressSN2,pressSN3,pressure);
+
+  //read pressure transducers
+  readAllPress (pressureSerial,pressSN0,pressSN1,pressSN2,pressSN3,pressure);
   Serial.print(pressure[0]);
   Serial.print('\t');
-  Serial.println(pressure[1]);
-  //read temperature of each pressure transducer
+  Serial.print(pressure[1]);
 
   //read magnetometer
-
+  readMagnetometer(magSerial,magReading);
+  Serial.print('\t');
+  Serial.print(magReading[0]);
+  Serial.print('\t');
+  Serial.print(magReading[1]);
+  Serial.print('\t');
+  Serial.print(magReading[2]);
+  
   //read gyro
 
   //read accel
@@ -52,9 +59,25 @@ void loop() {
   //format packet to be sent to SD card
 
   //write to SD card
+  Serial.println();
   delay(10);
 }
 
+void readMagnetometer(USARTClass &magSerial,int16_t *magReading)
+{
+  magSerial.print("*99P\r"); //TODO: add checking if data is actually available for all sensors
+  if (magSerial.available()>0)
+  {
+    byte buff[70];
+    for (int i=0;i<7;i++)
+    {
+      buff[i]=magSerial.read(); 
+    }
+    magReading[0] = buff[1]  + (buff[0] << 8);
+    magReading[1] = buff[3] + (buff[2] << 8);
+    magReading[2] = buff[5] + (buff[4]  << 8);
+  }
+}
 void readAllPress (USARTClass &pressureSerial,char add0[], char add1[], char add2[], char add3[], int16_t *pressure)
 {
   pressureSerial.print("WC\r");
@@ -66,9 +89,11 @@ void readAllPress (USARTClass &pressureSerial,char add0[], char add1[], char add
 
 int16_t readUniquePress(USARTClass &pressureSerial,char address[])
 {
-  char bytesIn[80]={0x00};
-  char readComm[80]={0x00};
-  
+  char bytesIn[80]={
+    0x00  };
+  char readComm[80]={
+    0x00  };
+
   int nchars;
   strcat(readComm,"U");
   strcat(readComm,address);
@@ -131,6 +156,7 @@ void serialDeviceInit(UARTClass& mainSerial, USARTClass& deviceSerial, int devic
     mainSerial.println(" serial port not responding.");
   }
 }
+
 
 
 
