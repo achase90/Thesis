bbb6f9d5bd6e5dec1d3ca51bb87694d001128b8f
Adam Chase <achase90@gmail.com>
2013-08-01 14:21:31 -0700

    I think gps is working now

--- hardware/GPS/parser/parser.ino
+++ hardware/GPS/parser/parser.ino
@@ -1,79 +1,198 @@
 
+
+
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
+  Serial2.flush();
+  Serial2.setTimeout(200);
 }
 
 void loop() {
-
+  char bytesIn[80] = {
+    0x00    };
+  int nchars;
   if (Serial2.available()>0)
   {
-    char bytesIn[64] = {0x00};
-    char msgID[5] = {0x00};
-    char utcTime[9] = {0x00};
-    char gpsStatus[1] = {0x00};
-    char lat[9] = {0x00};
-    char nsInd[1] = {0x00};
-    char gpsLong[10] = {0x00};
-    char ewInd[1] = {0x00};
-    char spd[10] = {0x00};
-    char cog[10] = {0x00};
-    char date[6] = {0x00};
-    char mode[1] = {0x00};
-    char CS[3] = {0x00};
+    if (Serial2.read() == '$')
+    {
+      char msgID[5];
+      uint32_t utcTime;
+      char gpsStatus;
+      int32_t gpsLat;
+      char nsInd;
+      int32_t gpsLong;
+      char ewInd;
+      int32_t gpsSpd;
+      int32_t gpsCrs;
+      uint32_t date;
+      char mode;
+      uint32_t CS;
 
-    int nchars;
-    nchars = Serial2.readBytesUntil('$',bytesIn,64); //read until beginning of GPS string
-    nchars = Serial2.readBytesUntil(',',msgID,64);
-    Serial.write((byte *)msgID,nchars);
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',utcTime,64); 
-    Serial.print(uint32_t (1000*atof(utcTime)));
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',gpsStatus,64);
-    Serial.write((byte *)gpsStatus,nchars);
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',lat,64);
-    Serial.print(int32_t (100000*atof(lat)));
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',nsInd,64);
-    Serial.write((byte *)nsInd,nchars);
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',gpsLong,64);
-    Serial.print(uint32_t (100000*atof(gpsLong)));
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',ewInd,64);
-    Serial.write((byte *)ewInd,nchars);
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',spd,64);
-    Serial.print(int32_t (10000*atof(spd)));
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',cog,64);
-    Serial.print(int32_t (10000*atof(cog)));
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',date,64);
-    Serial.print(uint32_t (atoi(date)));
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil(',',bytesIn,64);
-    nchars = Serial2.readBytesUntil(',',bytesIn,64);
-    nchars = Serial2.readBytesUntil('*',mode,64);
-    Serial.write((byte *)mode,nchars);
-    Serial.print('\t');
-    nchars = Serial2.readBytesUntil('\r',CS,64);
-    Serial.println(uint32_t (atoi(CS)));
+      nchars = Serial2.readBytesUntil(0x0a,bytesIn,128);
+      //process gps string
+      char *s1=bytesIn,*pt;
+      pt = strsep(&s1,",*");
+      for (int j=0;j<5;j++)
+      {
+        msgID[j] = pt[j];
+      }
+      //Serial.print(msgID);
+      //Serial.print('\t'); 
+      int i=0;
+      while (pt = strsep( &s1,",*"))
+      {
+        //delay(5);
+        // Serial.println(i);
+        switch (i++)
+        {
+        case 0:
+          {
+            utcTime = 100*atof(pt);
+            // Serial.print(utcTime);
+            //Serial.print(pt);
+            // Serial.print('\t');
+            break;
+          }
+        case 1:
+          {
+            // Serial.print(pt);
+            //Serial.print('\t'); 
+            gpsStatus = *pt;
+            break;
+          }
+        case 2:
+          {                    
+            //Serial.print(pt);
+            //Serial.print('\t'); 
+            gpsLat = 100000*atof(pt);
+            break;
+          }
+        case 3:
+          {                    
+            //Serial.print(pt);
+            //Serial.print('\t'); 
+            nsInd = *pt;
+            break;
+          }
+        case 4:
+          {                    
+            //Serial.print(pt);
+            //Serial.print('\t'); 
+            gpsLong = 100000*atof(pt);
+            break;
+          }
+        case 5:
+          {                    
+            //Serial.print(pt);
+            // Serial.print('\t'); 
+            ewInd = *pt;
+            break;
+          }
+        case 6:
+          {                    
+            // Serial.print(pt);
+            // Serial.print('\t'); 
+            gpsSpd = 1000*atof(pt);
+            break;
+          }
+        case 7:
+          {                    
+            // Serial.print(pt);
+            // Serial.print('\t'); 
+            gpsCrs = 1000*atof(pt);
+            break;
+          }
+        case 8:
+          {                    
+            // Serial.print(pt);
+            // Serial.print('\t'); 
+            date = atoi(pt);
+            break;
+          }
+        case 9:
+          {                    
+            // Serial.print(pt);
+            // Serial.print('\t'); 
+            //magnetic variation, not available
+            break;
+          }
+        case 10:
+          {                    
+            // Serial.print(pt);
+            // Serial.print('\t'); 
+            //magnetic variation indicator, not available
+            break;
+          }
+        case 11:
+          {                    
+            // Serial.print(pt);
+            // Serial.print('\t'); 
+            mode = *pt;
+            break;
+          }
+        case 12:
+          {                    
+            // Serial.print(pt);
+            // Serial.print('\t'); 
+            CS = atoi(pt);
+            break;
+          }
+        }
+        //Serial.print(pt);
+        // Serial.print('\t'); 
 
-    Serial2.read(); //remove line feed character
+        // pt = strsep(NULL,",*");
+        //      Serial.write((byte *) bytesIn,nchars);
+      }
+      if (true)
+      {
+        Serial.print(msgID);
+        Serial.print('\t'); 
+
+        Serial.print(utcTime);
+        Serial.print('\t'); 
+
+        Serial.print(gpsStatus);
+        Serial.print('\t'); 
+
+        Serial.print(gpsLat);
+        Serial.print('\t'); 
+
+        Serial.print(nsInd);
+        Serial.print('\t'); 
+
+        Serial.print(gpsLong);
+        Serial.print('\t'); 
+
+        Serial.print(ewInd);
+        Serial.print('\t'); 
+
+        Serial.print(gpsSpd);
+        Serial.print('\t'); 
+
+        Serial.print(gpsCrs);
+        Serial.print('\t'); 
+
+        Serial.print(date);
+        Serial.print('\t'); 
+
+        Serial.print(mode);
+        Serial.print('\t');
+
+        Serial.println(CS);
+      }
+    }
+
+
+    // if the character that was read then flush the rest of the data
+    else
+    {
+      while (Serial2.available()>0)
+      {
+        Serial2.read(); 
+      }
+    }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
 
